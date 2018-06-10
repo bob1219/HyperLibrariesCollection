ab6a196c4dc01ceedbe9ff1ed1beb7ed416814b1
吉田大輝 <bob181219@yahoo.co.jp>
2018-06-10 16:03:27 +0900

    Describe types.h

---
+++ C/types.h
@@ -0,0 +1,33 @@
+// Copyright 2018 Daiki Yoshida
+//
+// This file is part of HyperLibrariesCollection.
+//
+// HyperLibrariesCollection is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// HyperLibrariesCollection is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with HyperLibrariesCollection. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef HYPER_LIBRARIES_COLLECTION_C_TYPES_H
+#define HYPER_LIBRARIES_COLLECTION_C_TYPES_H
+
+typedef signed char byte;
+typedef unsigned char ubyte;
+
+typedef unsigned short int ushort;
+typedef unsigned long int ulong;
+typedef long long int llong;
+typedef unsigned long long int ullong;
+
+typedef unsigned int uint;
+
+typedef long double ldouble;
+
+#endif
